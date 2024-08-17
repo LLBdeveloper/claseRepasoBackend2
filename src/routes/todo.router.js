06dd5f922e0f06a194ac5ac03a61ab60d3d4189a
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-08-17 12:27:00 -0300

    newTODO

--- src/routes/todo.router.js
+++ src/routes/todo.router.js
@@ -8,8 +8,25 @@ router.get("/", async (req,res) => {
         const todos = await TodoModel.find().lean()
         res.render("todos", {todos})
     } catch (error) {
-        
+        res.status(500).send("Error interno")
     }
+})
+
+//Ruta para crear un nuevo "todo"
+router.post("/todos", async (req, res) => {
+    const {title, description} = req.body
+    try {
+        const nuevoTodo = new TodoModel({title, description})
+        await nuevoTodo.save()
+        res.redirect("/")
+    } catch (error) {
+        res.status(500).send("Error en el servidor al intentar enviar un todo")
+    }
+})
+
+//Ruta para renderizar la vista "new"
+router.get("/new", (req, res) => {
+    res.render("new")
 })
 
 export default router
