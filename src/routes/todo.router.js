f103243178482b3e9d0a9958f36e67b9ee11f7b0
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-08-17 13:34:22 -0300

    todo ok

--- src/routes/todo.router.js
+++ src/routes/todo.router.js
@@ -29,4 +29,26 @@ router.get("/new", (req, res) => {
     res.render("new")
 })
 
+router.post("/todos/:id/complete", async (req, res) => {
+    try {
+        const todo = await TodoModel.findById(req.params.id)
+        todo.completed = true
+        await todo.save()
+        res.redirect("/")
+    } catch (error) {
+        res.status(500).send("no completaste o error")
+    }
+})
+
+//Ruta para eliminar un todo
+
+router.post("/todos/:id/delete", async (req, res) => {
+    try {
+        await TodoModel.findByIdAndDelete(req.params.id)
+        res.redirect("/")
+    } catch (error) {
+        res.status(500).send("Error al borrar")
+    }
+})
+
 export default router
